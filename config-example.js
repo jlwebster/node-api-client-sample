8d4d27d6f261f0d48c0343b3f003e4a82b885141
John Webster <jlwebster@gmail.com>
2012-10-16 07:18:23 -0600

    Updating the config example

--- config-example.js
+++ config-example.js
@@ -5,3 +5,6 @@ exports.host = '<HOST>';
 exports.port = '<PORT>';
 exports.access_id = '<ACCESS ID>';
 exports.secret = '<SECRET>';
+exports.s3_key = '<S3_ACCESS_ID>';
+exports.s3_secret = '<S3_SECRET>';
+exports.s3_bucket = '<S3_BUCKET>';
